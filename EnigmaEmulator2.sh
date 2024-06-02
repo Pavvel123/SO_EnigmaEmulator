7bde066170503f640aad133c6aee61fc50fb8e7f
Paweł Kurpiewski <94716283+Pavvel123@users.noreply.github.com>
2024-06-03 00:14:49 +0200

    Added getopts in which you can choose rotors and reflector

--- EnigmaEmulator2.sh
+++ EnigmaEmulator2.sh
@@ -1,77 +1,111 @@
 #!/bin/bash
 
-# Define the alphabet and rotors
- alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-   rotor1="EKMFLGDQVZNTOWYHXUSPAIBRCJ"
-   rotor2="AJDKSIRUXBLHWTMCQGZNPYFVOE"
-   rotor3="BDFHJLCPRTXVZNYEIWGAKMUSQO"
-reflector="YRUHQSLDPXNGOKMIEBFZCWVJAT"
+PassThroughRotor()
+{
+    index=$(( ( $(echo $alphabet | grep -aob "$char" | cut -d: -f1) + position[$1] ) % 26 ))
+    char=${rotors[$1]:$index:1}
+    index=$(( ( $(echo $alphabet | grep -aob "$char" | cut -d: -f1) - position[$1] + 26 ) % 26 ))
+    char=${alphabet:$index:1}
+}
 
-# Define the rotor positions (A=0, B=1, ..., Z=25)
-position1=0
-position2=0
-position3=0
+PassThroughRotorReverse()
+{
+    index=$(( $(echo $alphabet | grep -aob "$char" | cut -d: -f1)))
+    char=${alphabet:$((($index + ${position[$1]}) % 26)):1}
+    index=$(( $(echo ${rotors[$1]} | grep -aob "$char" | cut -d: -f1)))
+    char=${alphabet:$((($index - ${position[$1]} + 26) % 26)):1}
+}
+
+Help()
+{
+    echo "Help"
+}
+
+Version()
+{
+    echo v1.0
+}
+
+ChooseRotors()
+{
+    r1="rotor$1"
+    r2="rotor$2"
+    r3="rotor$3"
+    rotors=(${!r1} ${!r2} ${!r3})
+    echo ${rotors[0]}
+    echo ${rotors[1]}
+    echo ${rotors[2]}
+}
+
+ChooseReflector()
+{
+    ref="reflector$1"
+    reflector=${!ref}
+    echo $reflector
+}
+
+# Define the alphabet and rotors
+  alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    rotor1="EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+    rotor2="AJDKSIRUXBLHWTMCQGZNPYFVOE"
+    rotor3="BDFHJLCPRTXVZNYEIWGAKMUSQO"
+    rotor4="ESOVPZJAYQUIRHXLNFTGKDCMWB"
+    rotor5="VZBRGITYUPSDNHLXAWMJQOFECK"
+reflectorB="YRUHQSLDPXNGOKMIEBFZCWVJAT"
+reflectorC="FVPJIAOYEDRZXWGCTKUQSBNMHL"
+
+rotors=($rotor1 $rotor2 $rotor3)
+reflector=$reflectorB
+
+while getopts hvr:u: OPT; do
+    case $OPT in
+        h) Help;;
+        v) Version;;
+        r) ChooseRotors ${OPTARG:0:1} ${OPTARG:1:1} ${OPTARG:2:1};;
+        u) ChooseReflector $OPTARG;;
+        *) echo "unknown option";;
+    esac
+done
+
+# Define the rotor positions (A=0 B=1  Z=25)
+position=(0 0 0)
 
 # Read the message from the user
 read -p "Enter the message to encrypt: " message
 
 # Prepare the message
-message=$(echo $message | tr 'a-z' 'A-Z' | tr -d ' ')
+message=$(echo $message | tr 'a-z' 'A-Z')
 encrypted_message=""
 
 # Encrypt the message character by character
 for (( i=0; i<${#message}; i++ )); do
     char=${message:$i:1}
+    if [[ $char != [A-Z] ]]; then
+        encrypted_message+=$char
+        continue
+    fi
     
     # Rotate the rotors
-    if [ $position2 -eq 4 ]; then
-        position2=5
-        position1=$(( (position1 + 1) % 26 ))
+    if [ ${position[1]} -eq 4 ]; then
+        position[1]=5
+        position[0]=$(( (position[0] + 1) % 26 ))
     fi
-    position3=$(( (position3 + 1) % 26 ))
-    if [ $position3 -eq 22 ]; then
-        position2=$(( (position2 + 1) % 26 ))
+    position[2]=$(( (position[2] + 1) % 26 ))
+    if [ ${position[2]} -eq 22 ]; then
+        position[1]=$(( (position[1] + 1) % 26 ))
     fi
 
-    # Pass through rotor 3
-    index=$(( ( $(echo $alphabet | grep -aob "$char" | cut -d: -f1) + position3 ) % 26 ))
-    char=${rotor3:$index:1}
-    index=$(( ( $(echo $alphabet | grep -aob "$char" | cut -d: -f1) - position3 + 26 ) % 26 ))
-    char=${alphabet:$index:1}
-
-    # Pass through rotor 2
-    index=$(( ( $(echo $alphabet | grep -aob "$char" | cut -d: -f1) + position2 ) % 26 ))
-    char=${rotor2:$index:1}
-    index=$(( ( $(echo $alphabet | grep -aob "$char" | cut -d: -f1) - position2 + 26 ) % 26 ))
-    char=${alphabet:$index:1}
-
-    # Pass through rotor 1
-    index=$(( ( $(echo $alphabet | grep -aob "$char" | cut -d: -f1) + position1 ) % 26 ))
-    char=${rotor1:$index:1}
-    index=$(( ( $(echo $alphabet | grep -aob "$char" | cut -d: -f1) - position1 + 26 ) % 26 ))
-    char=${alphabet:$index:1}
+    PassThroughRotor 2
+    PassThroughRotor 1
+    PassThroughRotor 0
 
     # Pass through the reflector
     index=$(echo $alphabet | grep -aob "$char" | cut -d: -f1)
     char=${reflector:$index:1}
 
-    # Pass back through rotor 1 in reverse
-    index=$(( $(echo $alphabet | grep -aob "$char" | cut -d: -f1)))
-    char=${alphabet:$((($index + $position1) % 26)):1}
-    index=$(( $(echo $rotor1 | grep -aob "$char" | cut -d: -f1)))
-    char=${alphabet:$((($index - $position1 + 26) % 26)):1}
-
-    # Pass back through rotor 2 in reverse
-    index=$(( $(echo $alphabet | grep -aob "$char" | cut -d: -f1)))
-    char=${alphabet:$((($index + $position2) % 26)):1}
-    index=$(( $(echo $rotor2 | grep -aob "$char" | cut -d: -f1)))
-    char=${alphabet:$((($index - $position2 + 26) % 26)):1}
-
-    # Pass back through rotor 3 in reverse
-    index=$(( $(echo $alphabet | grep -aob "$char" | cut -d: -f1)))
-    char=${alphabet:$((($index + $position3) % 26)):1}
-    index=$(( $(echo $rotor3 | grep -aob "$char" | cut -d: -f1)))
-    char=${alphabet:$((($index - $position3 + 26) % 26)):1}
+    PassThroughRotorReverse 0
+    PassThroughRotorReverse 1
+    PassThroughRotorReverse 2
 
     # Append the encrypted character to the result
     encrypted_message+=$char
